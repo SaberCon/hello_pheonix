14189d1c1108fdf5a9c6b97e1601ca7425c8ab97
SaberCon <894876785@qq.com>
2022-09-24 19:16:40 +0800

    Update DB config

--- config/dev.exs
+++ config/dev.exs
@@ -2,8 +2,8 @@ import Config
 
 # Configure your database
 config :hello, Hello.Repo,
-  username: "postgres",
-  password: "postgres",
+  username: "root",
+  password: "root",
   hostname: "localhost",
   database: "hello_dev",
   stacktrace: true,
